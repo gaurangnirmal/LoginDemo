5627ce2493b3a3ac600413e9e8180c58ab765df3
mayur2871 <mayur2871@gmail.com>
2018-03-24 17:55:47 +0530

    Login demo with shared prefrences

---
+++ LoginDemo/LoginDemo/Helper/Constant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoginDemo.Helper
+{
+    public static class Constants
+    {
+        #region SharedPrefrenceKeys
+
+        public const string IsLoginKey = "IsLoginKey";
+
+        public const string IsCompanyOrgSelectedKey = "IsCompanyOrgSelectedKey";
+
+        public const string UserNameKey = "UserNameKey";
+
+        public const string EmailKey = "EmailKey";
+
+        public const string NameKey = "NameKey";
+
+        public const string UserDataKey = "UserDataKey";
+
+        #endregion
+    }
+}
